c64e11765fe734064ca7a2408233ffc5bd6a4870
Scott PC UK <40214330@live.napier.ac.uk>
2020-01-30 23:07:21 +0000

    code cleanup

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use (express.json());
 
-const uri = process.env.ATLAS_URI;
+
 mongoose.connect('mongodb://dbUser:dbUser@cluster0-shard-00-00-4xqf8.gcp.mongodb.net:27017,cluster0-shard-00-01-4xqf8.gcp.mongodb.net:27017,cluster0-shard-00-02-4xqf8.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 
