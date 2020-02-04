7cb2d75fe4ba0454c7fc2b42c7098bfd02f0a8c4
Scott PC UK <40214330@live.napier.ac.uk>
2020-02-04 01:35:10 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use (express.json());
 
 
+
 mongoose.connect('mongodb://dbUser:dbUser@cluster0-shard-00-00-4xqf8.gcp.mongodb.net:27017,cluster0-shard-00-01-4xqf8.gcp.mongodb.net:27017,cluster0-shard-00-02-4xqf8.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 
@@ -20,6 +21,24 @@ connection.once('open', () => {
 
     console.log("MongoDB connection established");
 })
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+    // intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+        // respond with 200
+        console.log("Allowing OPTIONS");
+        res.sendStatus(200);
+    } else {
+        // move on
+        next();
+    }
+});
+
+
 
 const jobRouter = require('./routes/jobs');
 const userRouter = require('./routes/users');
